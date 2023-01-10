1164eb83dc3aecb692114e7cc0698059990ac861
namaek_2 <science4588@gmail.com>
2023-01-11 00:12:55 +0900

    infowindow 구현중

--- public/data/data.js
+++ public/data/data.js
@@ -2,15 +2,103 @@ var data = [
   {
     title: "서울",
     content: "서울 남산",
-    date: "2020-07-10",
+    mount: 100000,
     lat: 37.550434,
     lng: 126.989704,
   },
   {
     title: "서울",
     content: "서울 용산",
-    date: "2020-07-11",
+    mount: 500,
     lat: 37.53104,
     lng: 126.978978,
+  },
+
+  {
+    title: "서울",
+    content: "서울 어딘가",
+    mount: 500,
+    lat: 37.54104,
+    lng: 126.958978,
+  },
+
+  {
+    title: "서울",
+    content: "서울 광화문",
+    mount: 1,
+    lat: 37.555869,
+    lng: 126.956859,
+  },
+
+  {
+    title: "서울",
+    content: "서울 광화문",
+    mount: 1,
+    lat: 37.555871,
+    lng: 126.956859,
+  },
+
+  {
+    title: "서울",
+    content: "서울 광화문",
+    mount: 1,
+    lat: 37.555873,
+    lng: 126.956865,
+  },
+
+  {
+    title: "서울",
+    content: "서울 광화문",
+    mount: 1,
+    lat: 37.555875,
+    lng: 126.956863,
+  },
+
+  {
+    title: "서울",
+    content: "서울 광화문",
+    mount: 1,
+    lat: 37.555877,
+    lng: 126.956861,
+  },
+
+  {
+    title: "서울",
+    content: "서울 광화문",
+    mount: 1,
+    lat: 37.555879,
+    lng: 126.956867,
+  },
+
+  {
+    title: "서울",
+    content: "서울 광화문",
+    mount: 1,
+    lat: 37.555875,
+    lng: 126.956859,
+  },
+
+  {
+    title: "서울",
+    content: "서울 광화문",
+    mount: 1,
+    lat: 37.555876,
+    lng: 126.956859,
+  },
+
+  {
+    title: "서울",
+    content: "서울 광화문",
+    mount: 1,
+    lat: 37.555877,
+    lng: 126.956859,
+  },
+
+  {
+    title: "서울",
+    content: "서울 광화문",
+    mount: 1,
+    lat: 37.555878,
+    lng: 126.956859,
   },
 ];
